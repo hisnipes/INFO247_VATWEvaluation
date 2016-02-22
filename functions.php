79ad1614632a55a495c5461e7a00ae4b100f60a6
Steph Snipes <ssnipes@berkeley.edu>
2016-02-22 13:24:45 -0800

    Added validation hook

--- functions.php
+++ functions.php
@@ -16,12 +16,15 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 // custom functions created for form ids 1 and 2
 // form id 1 = identification (student picks their last name)
 // form id 2 = survey questions
-// gform_pre_render_### | ### is the form id
+// gform_pre_render_### | ### is the form id (leave out to apply to all forms)
+// gform_validation_### | ### is the form id (leave out to apply to all forms)
 // gform_field_value_xxx | xxx is the field parameter name
 add_filter('gform_pre_render_1', 'selectstudent');
-add_filter( 'gform_pre_validation', 'selectstudent' );
-add_filter( 'gform_admin_pre_render', 'selectstudent' );
-add_filter( 'gform_pre_submission_filter', 'selectstudent' );
+add_filter('gform_pre_validation', 'selectstudent');
+add_filter('gform_admin_pre_render', 'selectstudent');
+add_filter('gform_pre_submission_filter', 'selectstudent');
+
+add_filter('gform_validation_1', 'custom_validation');
 
 add_filter('gform_pre_render_2', 'populate_vizimg');
 
@@ -42,6 +45,7 @@ $json = json_decode($file);
 $rows = $json->{'feed'}->{'entry'};
 
 // initialize arrays
+$digits = array();
 $students = array();
 $firsts = array();
 $q1s = array();
@@ -52,6 +56,7 @@ $records = array();
 
 // for each row of data in the spreadsheet, separate them out by column name
 foreach($rows as $row) {
+	$digit = $row->{'gsx$digits'}->{'$t'};
 	$student = $row->{'gsx$lastname'}->{'$t'};
 	$first = $row->{'gsx$firstname'}->{'$t'};
 	$questions1 = $row->{'gsx$questions1'}->{'$t'};
@@ -60,6 +65,7 @@ foreach($rows as $row) {
 	$he2 = $row->{'gsx$he2'}->{'$t'};
 
 	// save each piece of data inside each array
+	$digits[] = $digit;
 	$students[] = $student;
 	$firsts[] = $first;
 	$q1s[] = $questions1;
@@ -71,11 +77,12 @@ foreach($rows as $row) {
 // create one big array with the Key being the student's last name and
 // and the Value as an array of values
 $records = array_merge_recursive(
-	array_combine($students, $firsts),
-	array_combine($students, $q1s),
-	array_combine($students, $q2s),
-	array_combine($students, $h1),
-	array_combine($students, $h2)
+	array_combine($students, $digits), // $value[0]
+	array_combine($students, $firsts), // $value[1]
+	array_combine($students, $q1s),	   // $value[2]
+	array_combine($students, $q2s),    // $value[3]
+	array_combine($students, $h1),     // $value[4]
+	array_combine($students, $h2)      // $value[5]
 );
 
 // function to pre-populate dropdown menu in form id 1
@@ -103,6 +110,48 @@ function selectstudent($form, $records){
 	return $form;
 }
 
+// validate against last four digits
+
+function custom_validation($validation_result) {
+	global $records;
+
+	$form = $validation_result['form'];
+
+	foreach($form['fields'] as &$field){
+		if(strpos($field->cssClass,'studentid') === false)
+			continue;
+		$visitor_name = rgpost("input_{$field['id']}");
+	}
+
+	foreach($form['fields'] as &$field){
+		if(strpos($field->cssClass,'sid-digits') === false)
+			continue;
+
+		$field_value = rgpost("input_{$field['id']}");
+
+		foreach($records as $key => $value){
+			if($key == $visitor_name)
+				$visitor_digits = $value[0];
+		}
+
+		if ( $field_value === $visitor_digits )
+			$is_valid = true;
+		else
+			$is_valid = false;
+
+		if ( !$is_valid ) {
+			$validation_result['is_valid'] = false;
+
+			$field->failed_validation = true;
+			$field->validation_message = "Sorry, those aren't the right digits. If this is incorrect, please contact your TA.";
+		}
+	}
+
+	$validation_result['form'] = $form;
+
+	return $validation_result;
+}
+
 // function to pre-populate viz imgs in the custom html fields
 // this code is kind of spaghetti-ish, def. could be improved later
 function populate_vizimg($form){
@@ -115,7 +164,7 @@ function populate_vizimg($form){
 		if($field->id == 6) {
     		foreach($records as $key => $value) {
      			if($key == $studentID)
-    	 			$field->content = '<label class="gfield_label">Visualization #1</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[1].'.jpg"><br/><br/>';
+    	 			$field->content = '<label class="gfield_label">Visualization #1</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[2].'.jpg"><br/><br/>';
     	 	};
      	};
 
@@ -123,7 +172,7 @@ function populate_vizimg($form){
      	if($field->id == 33) {
     		foreach($records as $key => $value) {
      			if($key == $studentID)
-    	 			$field->content = '<label class="gfield_label">Visualization #2</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[3].'.jpg"><br/><br/>';
+    	 			$field->content = '<label class="gfield_label">Visualization #2</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[4].'.jpg"><br/><br/>';
     	 	};
      	};
 
@@ -131,7 +180,7 @@ function populate_vizimg($form){
 		if($field->id == 7) {
     		foreach($records as $key => $value) {
      			if($key == $studentID)
-    	 			$field->content = '<label class="gfield_label">Visualization #3</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[2].'.jpg"><br/><br/>';
+    	 			$field->content = '<label class="gfield_label">Visualization #3</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[3].'.jpg"><br/><br/>';
     	 	};
      	};
 
@@ -139,7 +188,7 @@ function populate_vizimg($form){
      	if($field->id == 34) {
     		foreach($records as $key => $value) {
      			if($key == $studentID)
-    	 			$field->content = '<label class="gfield_label">Visualization #4</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[4].'.jpg"><br/><br/>';
+    	 			$field->content = '<label class="gfield_label">Visualization #4</label><br/><img src="'.get_stylesheet_directory_uri()."/viz/".$value[5].'.jpg"><br/><br/>';
     	 	};
      	};
 
@@ -157,7 +206,7 @@ function populate_viz1field(){
 
 	foreach($records as $key => $value){
 		if($key == $studentID)
-			return $value[1];
+			return $value[2];
 	};
 }
 
@@ -168,7 +217,7 @@ function populate_viz2field(){
 
 	foreach($records as $key => $value){
 		if($key == $studentID)
-			return $value[2];
+			return $value[3];
 	};
 }
 
@@ -186,7 +235,7 @@ function set_custom_conditionals($form) {
 
 	foreach($records as $key => $value){
 		if($key == $studentID)
-			$welcomename = $value[0]." ".$key;
+			$welcomename = $value[1]." ".$key;
 	};
 
 	echo "<h3><strong>Survey for:</strong> ".$welcomename."</h3>";
